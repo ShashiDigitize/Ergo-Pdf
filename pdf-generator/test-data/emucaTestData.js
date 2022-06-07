643a0a91bf8b0e0711fb852f8dc2c748008dbe94
KritikaDigitiz <kritikav@digitizeco.in>
2022-06-07 10:56:49 +0530

    first commit

---
+++ pdf-generator/test-data/emucaTestData.js
@@ -0,0 +1,68 @@
+const testData = {
+    "data": {
+    //     "thumbnail": "https://preview.threekit.com/api/files/hash/sha256-333d4bf021d9bab86fa8ed9fd37fb1a552988c6bcb0741a5f88babc4638cdf3d",
+    //     "Price": 855,
+    //     "valueTable": [
+    //         {
+    //             "name": "Chair Type",
+    //             "value": "Lagos"
+    //         },
+    //         {
+    //             "name": "Frame",
+    //             "value": "Black"
+    //         },
+    //         {
+    //             "name": "Fabric",
+    //             "value": "Montgomery Sterling"
+    //         },
+    //         {
+    //             "name": "Cylinder",
+    //             "value": "Low"
+    //         },
+    //         {
+    //             "name": "Fabric Type",
+    //             "value": "Montgomery Grade A"
+    //         },
+    //         {
+    //             "name": "Arms Options",
+    //             "value": "4D"
+    //         },
+    //         {
+    //             "name": "Mechanism",
+    //             "value": "LT control"
+    //         },
+    //         {
+    //             "name": "Seat Size",
+    //             "value": "Standard"
+    //         },
+    //         {
+    //             "name": "Foam",
+    //             "value": "Molded Seat"
+    //         },
+    //         {
+    //             "name": "Caster",
+    //             "value": "Carpet"
+    //         },
+    //         {
+    //             "name": "Base",
+    //             "value": "High Profile"
+    //         },
+    //         {
+    //             "name": "Head Rest",
+    //             "value": "None"
+    //         },
+    //         {
+    //             "name": "Other Options",
+    //             "value": ""
+    //         },
+    //         {
+    //             "name": "Adjustable Lumbar Support",
+    //             "value": "Included"
+    //         }
+    //     ]
+    }
+  }
+  
+    export default testData;
+
+
---
+++ server.js
@@ -0,0 +1,113 @@
+import generatePdf from './pdf-generator/index.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import postmark from 'postmark';
+import morgan from 'morgan';
+import dotenv from "dotenv";
+
+//const express = require("express");
+//const cors = require("cors");
+//const morgan = require("morgan");
+//const axios = require("axios").default;
+//const bodyParser=require("body-parser");
+//const postmark =require("postmark");
+//require("dotenv").config();
+//const generatePdf=require(// from './pdf-generator/index.js';
+dotenv.config();
+const app = express();
+// create application/json parser
+var jsonParser = bodyParser.json({ limit: "50mb" });
+app.use(express.urlencoded({ extended: true }));
+
+// const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
+// const SENDER_EMAIL = 'kritikav@digitizeco.in';
+// const client = new postmark.ServerClient(process.env.SERVER_TOKEN);
+
+//digitize Code
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).send({ message: "server healthy!" });
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Make REST API Calls In updated Express!')
+
+})
+
+
+//function to get PDF---Post 
+app.post('/api/pdf/:template',jsonParser, async (req, res) => {
+  if (!req.body) return res.status(422).send();
+  if (!req.params.template)
+    return res.status(422).send({ message: 'missing PDF template' });
+
+  const template = req.params.template;
+  if (!(template === 'ergo') )
+    return res.status(422).send({ message: 'Template not found', status: false });
+
+  try {
+    const pdf = await generatePdf(req.params.template, req.body);
+    res.set(
+      Object.assign(
+        { 'Content-type': 'application/pdf' },
+        req.query.download === 'true'
+          ? {
+              'Content-Disposition': `attachment;filename=${
+                req.query.filename || 'threekit-configuration.pdf'
+              }`,
+            }
+          : {}
+      )
+    );
+    res.end(pdf);
+  } catch (e) {
+    console.log(e);
+    res.status(422).send(e);
+  }
+});
+
+//function to get PDF----Get
+app.get('/api/pdf/:template',jsonParser, async (req, res) => {
+  // if (!req.body) return res.status(422).send();
+  if (!req.params.template)
+    return res.status(422).send({ message: 'missing PDF template' });
+  const template = req.params.template;
+  if (!(template === 'ergo') )
+    return res.status(422).send({ message: 'Template not found', status: false });
+  try {
+    const pdf = await generatePdf(req.params.template);
+    res.set(
+      Object.assign(
+        { 'Content-type': 'application/pdf' },
+        req.query.download === 'true'
+          ? {
+              'Content-Disposition': `attachment;filename=${
+                req.query.filename || 'threekit-configuration.pdf'
+              }`,
+            }
+          : {}
+      )
+    );
+    res.end(pdf);
+  } catch (e) {
+    console.log(e);
+    res.status(422).send(e);
+  }
+});
+
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
