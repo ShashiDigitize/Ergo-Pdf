9f17072af6d2fc12cc27cc760b910ddf4fd59ffd
KritikaDigitiz <kritikav@digitizeco.in>
2022-06-07 16:21:01 +0530

    for server restart

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ var jsonParser = bodyParser.json({ limit: "50mb" });
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
+app.options('*', cors());
 // app.use(express.json());   
 
 app.get("/api/health", (req, res) => {
