7260fde550a3a07896725ce44ff72354059913a6
KritikaDigitiz <kritikav@digitizeco.in>
2022-06-07 16:25:09 +0530

    for server restart

--- server.js
+++ server.js
@@ -40,8 +40,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var jsonParser = bodyParser.json({ limit: "50mb" });
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
-
-app.options('*', cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use(express.json());   
 
 app.get("/api/health", (req, res) => {
