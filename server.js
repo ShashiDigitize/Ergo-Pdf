7e98befc0f685e4637ad6117977d3f4b8a7a350b
KritikaDigitiz <kritikav@digitizeco.in>
2022-06-07 16:14:40 +0530

    for server restart

--- server.js
+++ server.js
@@ -17,16 +17,10 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-// create application/json parser
-var jsonParser = bodyParser.json({ limit: "50mb" });
-app.use(express.urlencoded({ extended: true }));
-
-
 // const PORT = process.env.PORT || 80;
 const PORT = process.env.PORT || 3001;
-// const SENDER_EMAIL = 'kritikav@digitizeco.in';
-// const client = new postmark.ServerClient(process.env.SERVER_TOKEN);
-
+const SENDER_EMAIL = 'emuca@emuca.com';
+const client = new postmark.ServerClient(process.env.SERVER_TOKEN);
 //digitize Code
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -36,22 +30,33 @@ app.use(function(req, res, next) {
   next();
 });
 
+
+
 app.use(morgan("dev"));
-app.use(express.json());
+// app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+var jsonParser = bodyParser.json({ limit: "50mb" });
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+
+// app.use(express.json());   
 
 app.get("/api/health", (req, res) => {
   res.status(200).send({ message: "server healthy!" });
 });
 
 app.get('/', (req, res) => {
+
+    
   res.send('Welcome to Make REST API Calls In updated Express!')
 
 })
 
 
-//function to get PDF---Post 
+
+//function to get PDF
 app.post('/api/pdf/:template',jsonParser, async (req, res) => {
   if (!req.body) return res.status(422).send();
   if (!req.params.template)
@@ -82,11 +87,12 @@ app.post('/api/pdf/:template',jsonParser, async (req, res) => {
   }
 });
 
-//function to get PDF----Get
+//function to get PDF
 app.get('/api/pdf/:template',jsonParser, async (req, res) => {
   // if (!req.body) return res.status(422).send();
   if (!req.params.template)
     return res.status(422).send({ message: 'missing PDF template' });
+
   const template = req.params.template;
   if (!(template === 'ergo') )
     return res.status(422).send({ message: 'Template not found', status: false });
